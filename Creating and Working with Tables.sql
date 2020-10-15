61ecb42b68457a6436b5e2e23b8c421d497c203f
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-15 13:09:51 +0100

    adding a check constraint

--- Creating and Working with Tables.sql
+++ Creating and Working with Tables.sql
@@ -11,3 +11,22 @@ Gender nvarchar(50) NOT NULL
 --Adding a Foreign key Constraint
 Alter table tblPerson add constraint tblPerson_GenderID_FK
 Foreign Key (GenderId) references tblGender (ID)
+
+--Populating a Table--
+Insert into tblPerson (ID, Name, Email, GenderId) Values (7, 'Rich', 'r@r.com', 1)
+
+--Adding a default value(3) if none is provided in the GenderId column--
+ALTER TABLE tblPerson
+ADD CONSTRAINT DF_tblPerson_GenderId
+DEFAULT 3 FOR GENDERID
+
+--Dropping a Constraint
+ALTER TABLE tblPerson
+DROP CONSTRAINT DF_tblPerson_GenderId
+
+--Deleting a table row--
+Delete from tblPerson where ID = 4
+
+--Adding a check constraint for tblPerson table--
+ALTER TABLE tblPerson
+ADD CONSTRAINT CK_tblPerson_Age CHECK (AGE > 0 AND AGE < 150)
