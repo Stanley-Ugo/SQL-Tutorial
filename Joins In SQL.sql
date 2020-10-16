a5987e8f67a823207e024d9886877d94de0e2020
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-16 01:29:36 +0100

    Creating and seeding the tblDepartment table

--- Joins In SQL.sql
+++ Joins In SQL.sql
@@ -1,1 +1,12 @@
 --Joins In SQL SERVER--
+--Creating the Department table--
+Create Table tblDepartment
+(
+Id int Identity(1,1) NOT NULL Primary Key,
+DepartmentName nvarchar(20) NOT NULL,
+Location nvarchar(20) NOT NULL,
+DepartmentHead nvarchar(20) NOT NULL
+)
+
+--Seeding the Department data--
+Insert Into tblDepartment Values ('Other Departments','Sydney','Cindarella')
