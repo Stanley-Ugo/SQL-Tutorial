c9b8cfa37340a4322cac2a1a402a90193c1676d8
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-16 01:48:25 +0100

    creating tblEmployees & referencing the FK constraint to tblDepartment

--- Joins In SQL.sql
+++ Joins In SQL.sql
@@ -10,3 +10,16 @@ DepartmentHead nvarchar(20) NOT NULL
 
 --Seeding the Department data--
 Insert Into tblDepartment Values ('Other Departments','Sydney','Cindarella')
+
+Create Table tblEmployees
+(
+ID int Identity(1,1) NOT NULL Primary Key,
+Name nvarchar(20) NOT NULL,
+Gender nvarchar(20) NOT NULL,
+Salary int NOT NULL,
+DepartmentId int NUll
+)
+
+--Adding a Fk to reference tblDepartment table--
+Alter table tblEmployees add constraint tblEmployees_DepartmentId_FK
+Foreign Key (DepartmentId) references tblDepartment (ID)
