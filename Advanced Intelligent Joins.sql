8213cb592a07ef5f08b24d5ddb419f6296327d4f
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-17 05:30:07 +0100

    Getting only none matching Rows from the Left & RightTable

--- Advanced Intelligent Joins.sql
+++ Advanced Intelligent Joins.sql
@@ -14,3 +14,18 @@ from tblEmployees
 Left Join tblDepartment
 On tblEmployees.DepartmentId = tblDepartment.Id
 Where tblDepartment.Id Is NULL
+
+--Getting only the None matching rows from the right Table-
+Select Name, Gender, Salary, DepartmentName
+from tblEmployees
+Right Join tblDepartment
+On tblEmployees.DepartmentId = tblDepartment.Id
+Where tblEmployees.DepartmentId Is NULL
+
+--Getting only the None matching rows from the right & Left Table-
+Select Name, Gender, Salary, DepartmentName
+from tblEmployees
+Full Join tblDepartment
+On tblEmployees.DepartmentId = tblDepartment.Id
+Where tblEmployees.DepartmentId Is NULL
+Or tblDepartment.Id is NULL
