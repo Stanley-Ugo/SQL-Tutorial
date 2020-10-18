ea59d4800d09e085fcf2b420073e862c57a5a323
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-18 07:43:59 +0100

    Using the COALESCE() function to check null values

--- Replace Null in SQL Server.sql
+++ Replace Null in SQL Server.sql
@@ -1,8 +1,14 @@
 --Replace Null In SQL SERVER--
 Select * from tblEmployees1
 
---Replacing Null with thw ISNULL() Function--
+--Replacing Null with the ISNULL() Function--
 Select     E.Name as Employee, ISNULL(M.Name, 'No  Manager') as Manager
+from       tblEmployees1 E
+Left Join  tblEmployees1 M
+On         E.ManagerId = M.EmployeeId
+
+--Replacing Null with the COALESCE() Function--
+Select     E.Name as Employee, COALESCE(M.Name, 'No  Manager') as Manager
 from       tblEmployees1 E
 Left Join  tblEmployees1 M
 On         E.ManagerId = M.EmployeeId
