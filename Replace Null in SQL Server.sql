8649cfbf40cb6b6cfb57c2edb96fe72b691efe3a
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-18 07:40:11 +0100

    Using the ISNULL() function to check null values

--- Replace Null in SQL Server.sql
+++ Replace Null in SQL Server.sql
@@ -1,1 +1,8 @@
 --Replace Null In SQL SERVER--
+Select * from tblEmployees1
+
+--Replacing Null with thw ISNULL() Function--
+Select     E.Name as Employee, ISNULL(M.Name, 'No  Manager') as Manager
+from       tblEmployees1 E
+Left Join  tblEmployees1 M
+On         E.ManagerId = M.EmployeeId
