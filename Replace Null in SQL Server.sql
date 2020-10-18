47f188d10b6903906769bd818ce69400b29d519a
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-18 07:50:46 +0100

    using the CASE EXPRESSION

--- Replace Null in SQL Server.sql
+++ Replace Null in SQL Server.sql
@@ -12,3 +12,9 @@ Select     E.Name as Employee, COALESCE(M.Name, 'No  Manager') as Manager
 from       tblEmployees1 E
 Left Join  tblEmployees1 M
 On         E.ManagerId = M.EmployeeId
+
+--Replacing Null with the CASE EXPRESSION Function--
+Select     E.Name as Employee, CASE WHEN M.Name IS NULL THEN 'No Manager' ELSE M.Name END as Manager
+from       tblEmployees1 E
+Left Join  tblEmployees1 M
+On         E.ManagerId = M.EmployeeId
