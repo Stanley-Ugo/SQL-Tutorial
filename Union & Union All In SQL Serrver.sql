6cac26a99db336d820addadf374fcd729a0281d5
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-19 18:57:22 +0100

    adding both tblIndiaCustomers & tblUKCustomers sample tables

--- Union & Union All In SQL Serrver.sql
+++ Union & Union All In SQL Serrver.sql
@@ -1,1 +1,16 @@
 --Union & Union All In SQL SERVER--
+--Indian Customers Table--
+Create Table tblIndiaCustomers
+(
+Id int identity(1,1) Primary Key,
+Name nvarchar(20) NOT NULL,
+Email nvarchar(20) NOT NULL
+)
+
+--UK Customers Table--
+Create Table tblUKCustomers
+(
+Id int identity(1,1) Primary Key,
+Name nvarchar(20) NOT NULL,
+Email nvarchar(20) NOT NULL
+)
