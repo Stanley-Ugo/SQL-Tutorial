22dadf26e92ad72132b40037d90270108254e823
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-20 22:56:24 +0100

    creating a 'spGetEmployees' store procedure

--- Stored procedures inSQL Server.sql
+++ Stored procedures inSQL Server.sql
@@ -1,1 +1,10 @@
+--Stored Procedures In SQL Server--
 Select * from tblEmployees
+
+--Creating a stored procedure 'spGetEmployees' for getting Names,Gender from tblEmployees Table--
+CREATE PROCEDURE spGetEmployees
+AS
+BEGIN
+     Select Name, Gender from tblEmployees
+END
+
