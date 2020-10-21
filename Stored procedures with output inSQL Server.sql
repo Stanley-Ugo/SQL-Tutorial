725d16ac039e003de989b998f3458e42f136c268
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-21 15:48:45 +0100

    Creating spGetEmployeeCountByGender with output

--- Stored procedures with output inSQL Server.sql
+++ Stored procedures with output inSQL Server.sql
@@ -1,1 +1,12 @@
 --Stored Procedure with output--
+Select * from tblEmployees
+
+--Creating stored procedure for getting Employee count by Gender--
+Create Procedure spGetEmployeeCountByGender
+@Gender nvarchar(20),
+@EmployeeCount int output
+as
+Begin
+    Select @EmployeeCount = COUNT(ID) from tblEmployees where Gender = @Gender
+End
+
