ec2b17876390fd7943313e6ad5aa08df022faaa2
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-21 21:06:37 +0100

    Mathematical Function in SQL SERVER

--- Cast and Convert Functions in Sql Server.sql
+++ Cast and Convert Functions in Sql Server.sql
@@ -9,3 +9,31 @@ Select Id, Name, DateOfBirth, CONVERT(nvarchar, DateOfBirth) as ConvertedDOB fro
 
 --Concatenate ID(int)  and Name(nvarchar) with the CAST function--
 Select Id, Name, Name + ' - ' + CAST(Id as nvarchar) as [Name - Id] from tblEmployeesDate
+
+
+--MATHEMATICAL FUNCTION IN SQLServer--
+Select ABS(-105.1) -- returns the Absolute value 105.1 without the -negative sign
+--CEILING--
+Select CEILING(15.2) -- returns 16
+Select CEILING(-15.2) --returns -15
+--FLOOR--
+Select FLOOR(15.2) -- returns 15
+select FLOOR(-15.2) -- returns -16
+--POWER--
+Select POWER(2,3) -- returns 8
+--SQUARE--
+Select SQUARE(9) -- returns 81
+--SQRT--
+Select SQRT(9) -- returns the square root of 3
+--RAND--
+Select RAND() -- returns random numbers between 1 and 0
+Select RAND(1) -- returns the same number between 1 and 0
+Select FLOOR(RAND() * 100) -- returns random whole number from 1 to 100
+--Ten random Numbers between 1 - 100--
+Declare @Counter int
+Set @Counter = 1
+While (@Counter <= 10)
+Begin
+    Print FLOOR(RAND() * 100)
+	Set @Counter = @Counter + 1
+End
