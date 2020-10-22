e71ea79720ea8d0024150eded2bb974ff2584df4
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-22 23:43:05 +0100

    Insert | Update | Delete | table-Joins | View

--- Views In SQL SERVER.sql
+++ Views In SQL SERVER.sql
@@ -12,3 +12,26 @@ On tblEmployees.DepartmentId = tblDepartment.Id
 
 --Executing the View--
 Select * from vWEmployeesByDepartment
+
+--***************************************************--
+--UPDATEABLE VIEWS IN SQL Server--
+--Updates a table row
+Update vWEmployeesByDepartment
+Set Name = 'Mikey' Where Id = 2
+
+--Deletes a Table row--
+Delete from vWEmployeesByDepartment Where Id = 2
+
+--Inserts a table row with a view--
+Insert into vWEmployeesByDepartment values (2, 'Mikey','Male',2)
+
+--Creating a View which returns the result of the JOIN of two tables--
+Create view vwEmployeeDetailsByDepartment
+as
+Select Name, Salary, Gender, DepartmentName
+from tblEmployees
+JOIN tblDepartment
+ON tblEmployees.DepartmentId = tblDepartment.Id
+
+--Executing the View--
+Select * from vwEmployeeDetailsByDepartment
