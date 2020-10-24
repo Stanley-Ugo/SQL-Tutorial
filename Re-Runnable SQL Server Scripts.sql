dcbcf7af551bf8fa0b63d94f184298dd755f0123
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-24 15:00:52 +0100

    Re-Runnable SQL Server Scripts

---
+++ Re-Runnable SQL Server Scripts.sql
@@ -0,0 +1,20 @@
+--RE-RUNNABLE SQL SEREVR Scripts--
+--Creating a table twice in the same database is none re-runnable--
+
+--Making it re-runnable--
+Use [Sample]
+If not exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'tblEmployee')
+Begin
+    Create table tblEmployee
+    (
+       Id int identity primary Key,
+       Name nvarchar(100),
+       Gender nvarchar(10),
+       DateOfBirth DateTime
+    )
+	Print 'Table tblEmployee Successfully crreated'
+End
+Else
+Begin
+    Print 'Table tblEmployee already Exist'
+End
