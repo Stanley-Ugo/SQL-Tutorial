53d08b739ea0c7450b5721b36a419ec1d282561c
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-25 19:41:01 +0100

    Using the EXCEPT Operator

--- Handling Errors In ADO.NET Application.sql
+++ Handling Errors In ADO.NET Application.sql
@@ -5,3 +5,8 @@ DBCC OpenTran
 
 --Killing a Process in SQL Syntax-
 Kill ProcessId
+
+--Except operators in SQL--
+Select Id, Name, Gender from TableA
+EXCEPT
+Select Id, Name, Gender from TableB -- Returns rows that aren't on both tables
