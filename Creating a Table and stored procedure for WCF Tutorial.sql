094a697f98590c49cf8a228101496977b66b4103
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-01 06:07:47 +0100

    Ading a spGetEmployee storedProcedure

--- Creating a Table and stored procedure for WCF Tutorial.sql
+++ Creating a Table and stored procedure for WCF Tutorial.sql
@@ -12,3 +12,12 @@ Insert into tblEmployeeWCF Values (1, 'Mark', 'Male', '10/10/1980')
 Insert into tblEmployeeWCF Values (2, 'Mary', 'Female', '11/10/1981')
 Insert into tblEmployeeWCF Values (3, 'John', 'Male', '8/10/1979')
 
+--Creating a GetEmployeeWCF Stored procedure--
+Create procedure spGetEmployee
+@Id int
+as
+Begin
+    Select Id, Name, Gender, DateOfBirth
+	from tblEmployeeWCF
+	where Id = @Id
+End
