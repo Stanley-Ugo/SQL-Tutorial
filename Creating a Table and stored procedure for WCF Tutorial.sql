b135682906f4a44eb774ef37b6ee43adaeb91364
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-01 06:03:45 +0100

    Inmserting Values into tblEMployeeWCF

--- Creating a Table and stored procedure for WCF Tutorial.sql
+++ Creating a Table and stored procedure for WCF Tutorial.sql
@@ -6,3 +6,9 @@ Name nvarchar(50),
 Gender nvarchar(50),
 DateOfBirth datetime
 )
+
+--Inserting Values into tblEmployeeWCF Table--
+Insert into tblEmployeeWCF Values (1, 'Mark', 'Male', '10/10/1980')
+Insert into tblEmployeeWCF Values (2, 'Mary', 'Female', '11/10/1981')
+Insert into tblEmployeeWCF Values (3, 'John', 'Male', '8/10/1979')
+
