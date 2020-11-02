6179f08dacc1a2d9fcd5d396aeb4ebf7dc6c41be
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-02 23:17:15 +0100

    Altering spGetEmployee stored procedure

--- Creating a Table and stored procedure for WCF Tutorial.sql
+++ Creating a Table and stored procedure for WCF Tutorial.sql
@@ -13,19 +13,15 @@ Insert into tblEmployeeWCF Values (2, 'Mary', 'Female', '11/10/1981')
 Insert into tblEmployeeWCF Values (3, 'John', 'Male', '8/10/1979')
 
 --Creating a GetEmployeeWCF Stored procedure--
-Alter procedure spGetEmployee
-@Id int
-as
-Begin
-    Select Id, Name, Gender, DateOfBirth, EmployeeType, AnnualSalary, HourlyPay, HoursWorked
-	from tblEmployeeWCF
-	where Id = @Id
-End
+
 
 --Executing the Stored Procedure--
 Execute spGetEmployee '1'
 
-
+--This Clears table data incase of Mistakes to be reseeded--
+TRUNCATE Table tblEmployeeWCF
+--OR--
+DELETE from tblEmployeeWCF
 
 --Creating a spSaveEmpmployeeWCF store procedure--
 Alter procedure spSaveEmployeeWCF
