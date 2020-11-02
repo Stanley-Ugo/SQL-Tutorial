7ae5997fc355448818ec3edbbbf2ae933d1f0b1e
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-02 23:12:15 +0100

    Altering the Tables

--- Creating a Table and stored procedure for WCF Tutorial.sql
+++ Creating a Table and stored procedure for WCF Tutorial.sql
@@ -13,11 +13,11 @@ Insert into tblEmployeeWCF Values (2, 'Mary', 'Female', '11/10/1981')
 Insert into tblEmployeeWCF Values (3, 'John', 'Male', '8/10/1979')
 
 --Creating a GetEmployeeWCF Stored procedure--
-Create procedure spGetEmployee
+Alter procedure spGetEmployee
 @Id int
 as
 Begin
-    Select Id, Name, Gender, DateOfBirth
+    Select Id, Name, Gender, DateOfBirth, EmployeeType, AnnualSalary, HourlyPay, HoursWorked
 	from tblEmployeeWCF
 	where Id = @Id
 End
@@ -27,15 +27,27 @@ Execute spGetEmployee '1'
 
 --This Clears table data incase of Mistakes to be reseeded--
 TRUNCATE Table tblEmployeeWCF
+--OR--
+DELETE from tblEmployeeWCF
 
 --Creating a spSaveEmpmployeeWCF store procedure--
-Create procedure spSaveEmployeeWCF
+Alter procedure spSaveEmployeeWCF
 @Id int,
 @Name nvarchar(50),
 @Gender nvarchar(50),
-@DateOfBirth DateTime
+@DateOfBirth DateTime,
+@EmployeeType int,
+@AnnualSalary int = NULL,
+@HourlyPay int = NULL,
+@HoursWorked int = NULL
 as
 Begin
     Insert into tblEMployeeWCF
-	Values (@Id, @Name, @Gender, @DateOfBirth)
+	Values (@Id, @Name, @Gender, @DateOfBirth, @EmployeeType, @AnnualSalary, @HourlyPay, @HoursWorked)
 End
+
+--Adding a few more columns to the tblEmployeeWCF table--
+Alter table tblEmployeeWCF Add
+EmployeeType int, AnnualSalary int, HourlyPay int, HoursWorked int
+
+Select * from tblEmployeeWCF
