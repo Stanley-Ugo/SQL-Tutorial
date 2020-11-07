4e47e4412aa011d9e8edf1583b9616d25ddcd4fd
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-08 00:19:06 +0100

    Executing the Select all command

--- Creating a Table and stored procedure for WCF Tutorial.sql
+++ Creating a Table and stored procedure for WCF Tutorial.sql
@@ -65,3 +65,6 @@ DateOfBirth datetime
 Insert into tblEmployeeWCF Values (1, 'Mark', 'Male', '10/10/1980')
 Insert into tblEmployeeWCF Values (2, 'Mary', 'Female', '11/10/1981')
 Insert into tblEmployeeWCF Values (3, 'John', 'Male', '8/10/1979')
+
+--Executing the Select all Command--
+Select * from tblGreetingServiceIIS
