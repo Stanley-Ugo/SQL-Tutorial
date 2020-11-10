fddc957477abf73d0435ab9f57eee9df0e3bfbb1
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-10 08:09:16 +0100

    Adding a FK to a table

--- Creating and Working with Tables.sql
+++ Creating and Working with Tables.sql
@@ -8,7 +8,7 @@ ID int NOT NULL Primary Key,
 Gender nvarchar(50) NOT NULL
 )
 
---Adding a Foreign key Constraint
+--Adding a Foreign key Constraint--
 Alter table tblPerson add constraint tblPerson_GenderID_FK
 Foreign Key (GenderId) references tblGender (ID)
 
